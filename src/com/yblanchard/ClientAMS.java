d061816fc700dd3aafd5d60c0cc56d6355fadab0
Yann <yann.blanchard@u-paris10.fr>
2015-10-31 17:49:10 +0100

    ajout SystemManager

--- src/com/yblanchard/ClientAMS.java
+++ src/com/yblanchard/ClientAMS.java
@@ -4,5 +4,6 @@ public class ClientAMS {
 
     public static void main(String[] args) {
 	// write your code here
+        SystemManager res  = SystemManager.getInstance();
     }
 }
---
+++ src/com/yblanchard/SystemManager.java
@@ -0,0 +1,17 @@
+package com.yblanchard;
+
+/**
+ * Created by Yann on 31/10/2015.
+ */
+public class SystemManager {
+    private static SystemManager ourInstance = new SystemManager();
+
+    public static SystemManager getInstance() {
+        return ourInstance;
+    }
+
+    private SystemManager() {
+
+
+    }
+}
