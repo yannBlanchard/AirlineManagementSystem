d23b58a30407864dae3282ec5acb7694d2052583
Yann <yann.blanchard@u-paris10.fr>
2015-11-01 13:41:28 +0100

    création des class et de l'enumération

---
+++ src/com/yblanchard/Seat.java
@@ -0,0 +1,7 @@
+package com.yblanchard;
+
+/**
+ * Created by Yann on 01/11/2015.
+ */
+public class Seat {
+}
---
+++ src/com/yblanchard/SeatClass.java
@@ -0,0 +1,21 @@
+package com.yblanchard;
+
+/**
+ * Created by Yann on 01/11/2015.
+ */
+public enum SeatClass {
+    FIRST("First"),
+    BUSI("Business"),
+    ECO("Economy");
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private String name;
+
+}
